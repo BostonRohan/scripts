7533d32d08ef9e48ea1a6f650b08729f126d5d21
Boston <bostonrohan@gmail.com>
2024-06-22 19:12:37 -0400

    chore: remove axum

--- src/bin/courtneyrohan/wix-blog-migration/main.rs
+++ src/bin/courtneyrohan/wix-blog-migration/main.rs
@@ -1,11 +1,2 @@
-use axum::{routing::post, Router};
-mod wix_oauth;
-use crate::wix_oauth::webhook;
-
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", post(webhook));
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-}
+async fn main() {}
